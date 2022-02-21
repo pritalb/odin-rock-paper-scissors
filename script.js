1ff8ba4d513236166d704e4183239f10647f6eea
pritalb <pritalbamnodkar@gmail.com>
2022-02-21 21:40:54 +0530

    game function implemented. Project done.

--- script.js
+++ script.js
@@ -12,9 +12,11 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
 	let win = false;
 	let playerPlay = playerSelection.toLowerCase();
+	let result = {};
 
 	if (playerPlay === computerSelection) {
-		return  `Draw! Both players selected ${playerPlay}`;
+			result['result'] = `Draw! Both players selected ${playerPlay}`;
+			result['winner'] = 'no winner';
 	}
 
 	if ((playerPlay === 'rock' && computerSelection === 'scissor') || (playerPlay === 'paper' && computerSelection === 'rock') || (playerPlay === 'scissor' && computerSelection === 'paper')) {
@@ -22,16 +24,41 @@ function playRound(playerSelection, computerSelection) {
 	}
 
 	if (win) {
-		return `You Win! ${playerPlay} beats ${computerSelection}`;
+		result['result'] = `You Win! ${playerPlay} beats ${computerSelection}`;
+		result['winner'] = 'player';
+	} else {
+		result['result'] = `You Lose! ${computerSelection} beats ${playerPlay}`;
+		result['winner'] = 'computer';
 	}
-	return `You Lose! ${computerSelection} beats ${playerPlay}`;
+
+	return result
 }
 
 function game() {
+	let score = {
+		'player': 0,
+		'computer': 0,
+		'no winner': 0,
+	};
 
+	for (let index = 0; index < 5; index++) {
+		let player_hand = prompt(` Round ${index}: choose a hand (from: rock, paper or scissor).`);
+		let compPlay = computerPlay();
+
+		let round_result = playRound(player_hand, compPlay);
+		
+		score[round_result['winner']]++;
+		console.log(`Round ${index} result: ${round_result['result']}`);
+		console.log(`current score => player : ${score['player']}, computer : ${score['computer']}`);
+	}
+
+	if (score['player'] > score['computer']) {
+		console.log('Player wins.')
+	} else if (score['player'] < score['computer']) {
+		console.log('Computer wins.')
+	} else {
+		console.log('Game Draw.');
+	}
 }
 
-let playerPlay = 'Rock';
-let compPlay = computerPlay();
-
-console.log(playRound(playerPlay, compPlay))
+game();
